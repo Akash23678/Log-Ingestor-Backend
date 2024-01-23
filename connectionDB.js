bac8ac83aa4c19279e212c05cdcbb1588349c2d6
Akash <sahu69808@gmail.com>
2024-01-23 19:20:45 +0530

    Migrate MongoDB Compass into MongoDB Atlas(Cloud)

--- connectionDB.js
+++ connectionDB.js
@@ -4,8 +4,12 @@ const mongoose = require("mongoose")
 const connectMongoDB = async () => {
     
     try{
+        const URI = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PWD}@cluster0.p9mg7oq.mongodb.net/?retryWrites=true&w=majority`;
+        
+        const mongoConnection = await mongoose.connect(URI);
+        //const mongoConnection = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+        
         //const mongoConnection = await mongoose.connect(`mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@localhost:27017/${process.env.DB_NAME}`);
-        const mongoConnection = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         //const mongoConnection = await mongoose.connect(`mongodb://${process.env.USER_NAME}:${process.env.USER_PWD}@localhost:27017/${process.env.DB_NAME}`);
 
         console.log(`MongoDB Connected Successfully !!! DB Host:-> ${mongoConnection.connection.host} `)
